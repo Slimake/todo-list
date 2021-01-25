0a5df1d3760f91751aba8b0170a539e33581f27b
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-01-25 14:36:40 +0100

    add database

--- app.js
+++ app.js
@@ -1,47 +1,138 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-
-const items = [];
-
+ 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-	// Get request to root route 
-app.get("/", function(req, res) {
-	
-	const day = date.getDate();
+// Mongo Database using Mongoose for interaction
+mongoose.connect('mongodb+srv://admin-sunday:admin@cluster0.819nn.mongodb.net/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
-	res.render("list", {listTitle: day, newListItems: items});
+const itemsSchema = new mongoose.Schema({
+	name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item = new Item({
+	name: "Welcome to your todolist"
+});
+
+const defaultItem = [item];
+
+const listSchema = new mongoose.Schema({
+	name: String,
+	items: [itemsSchema]
+});
+
+const List = mongoose.model("List", listSchema);
+
+// Get request to root route 
+app.get("/", function(req, res) {
+
+	Item.find({}, function (err, foundItems) {
+		if (foundItems.length === 0) {
+			Item.insertMany(defaultItem, function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Successfully saved default items to DB");
+				}
+			});
+			setTimeout(function() {
+				res.redirect("/");
+			}, 1500);
+		} else {
+			res.render("list", {
+				listTitle: "Today", 
+				newListItems: foundItems
+			});	
+		}
+	});
+
+});
+
+app.get("/:customListName", function(req, res) {
+	const customListName = _.capitalize(req.params.customListName);
+
+	List.findOne({ name: customListName }, function(err, foundList) {
+		if (!err) {
+			if (!foundList) {
+				// Create a new list
+				const list = new List({
+					name: customListName,
+					items: defaultItem
+				});
+
+				list.save();
+				setTimeout(function() {
+					res.redirect("/" + customListName);
+				}, 1500);
+			} else {
+				// Show an existing list
+				res.render("list", {
+					listTitle: foundList.name, 
+					newListItems: foundList.items
+				});	
+			}
+		}
+	});
+
 });
 
 	// Post request to root route 
 app.post("/", function(req, res) {
-	const item = req.body.newItem;
+  	const itemName = req.body.newItem;
+  	const listName = req.body.list;
 
-	if (item) {
-		items.push(item);
+	if (itemName) {
+		// Adding new item to todolist Database
+		const item = new Item({
+			name: itemName
+		});
+
+		if (listName === "Today") {
+			item.save();
+			res.redirect("/");
+		} else {
+			List.findOne({ name: listName }, function(err, foundList) {
+				foundList.items.push(item);
+				foundList.save();
+				res.redirect("/" + listName);
+			});
+		}
 	}
 
-	res.redirect("/");
 });
 
-	// Remove an item from List
+// Remove an item from todolist Database
 app.post("/removeitem", function(req, res) {
-	const removeItem = req.body.itemRemove;
+	const removeItemId = req.body.itemRemove;
+	const listName = req.body.listName;
 
-	items.splice(items.indexOf(removeItem), 1);
-	res.redirect("/");
+	if (listName === "Today") {
+		Item.findByIdAndRemove(removeItemId, function(err) {
+			if (!err) {
+				console.log("Successfully deleted from item");
+				res.redirect("/");
+			}
+		});
+	} else {
+		List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: removeItemId } } }, function(err, foundList) {
+			if (!err) {
+				res.redirect("/" + listName);
+			}
+		});
+	}
 
 });
 
-	
 app.listen(process.env.PORT || 3000, function() {
 	console.log("Server started on port 3000") 
 });
